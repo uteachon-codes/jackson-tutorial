3b11b96811661038708433dee09ca024a56bf9e2
Hussain Ashruf <uteachon.codes@gmail.com>
2022-10-30 14:06:15 -0500

    JT-1 jackson-tutorial

---
+++ src/test/java/com/serverless/jackson/HandlerTest.java
@@ -0,0 +1,35 @@
+package com.serverless.jackson;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+class HandlerTest {
+
+    @InjectMocks
+    Handler handler;
+
+    void Handler() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void handleRequestTest() {
+
+        Map<String, Object> input = new HashMap<>();
+        input.put("key1", "value1");
+        input.put("key1", "value1");
+        input.put("key1", "value1");
+
+        handler = new Handler();
+        ApiGatewayResponse apiGatewayResponse = handler.handleRequest(input, null);
+        assertEquals(200, apiGatewayResponse.getStatusCode());
+    }
+}
